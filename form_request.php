ceeacdf3c2e826f03d6a920038045a4075ca4c13
lmoffat25 <leonmfft@gmail.com>
2024-07-15 14:00:56 +0200

    fix : nettoyé le code

--- form_request.php
+++ form_request.php
@@ -1,112 +1,90 @@
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    error_log('POST request received');
-    $api_key = get_option('option_api_key');
-    $version = get_option('option_version');
-    $summary = get_option('option_summary');
-    error_log('API Key: ' . $api_key);
-    error_log('Version: ' . $version);
-    error_log('Summary: ' . $summary);
-    error_log("GET ICI");
-    error_log('GET data: ' . print_r($_GET, true));
-    if ($_POST['action'] === 'blog_architect_handle_bulk_actions') {
-        if (isset($_POST['post_ids'])) {
-            $selected_posts = $_POST['post_ids'];
-            error_log('Selected Posts: ' . print_r($selected_posts, true));
-            update_categories_for_selected_posts($selected_posts);
-        }
-    }
-    if(!isset($_POST['post_ids'])){
-        error_log('empty');
-    }else{
-        $post_ids= $_POST['post_ids'];
+/**
+ * Gère les opérations pour un post donné.
+ *
+ * @param int $post_id ID du post.
+ * @param string $api_key Clé API.
+ * @param string $version Version.
+ * @param string $summary Résumé.
+ */
+function handle_post($post_id, $api_key, $version, $summary) {
+    $subject = get_the_title($post_id);
+    $context = get_post_meta($post_id, '_post_context', true) ?: '';
 
-        foreach ($post_ids as $post_id) {
-            $subject = get_the_title($post_id);
-            error_log('Post ID: ' . $post_id . ', Subject: ' . $subject);
-            $context = get_post_meta($post_id, '_post_context', true);
-            if (empty($context)) {
-                $context = "";
-            }
-            error_log('Post ID: ' . $post_id . ', Subject: ' . $subject . ', Context: ' . $context);
+    // Préparation des données pour l'API
+    $data = array(
+        'api_key' => $api_key,
+        'ndd' => parse_url(get_site_url(), PHP_URL_HOST),
+        'subject' => $subject,
+        'summary' => $summary,
+        'context' => $context,
+        'version' => $version
+    );
 
-            $data = array(
-                'api_key' => $api_key,
-                'ndd' => parse_url(get_site_url(), PHP_URL_HOST),
-                'subject' => $subject,
-                'summary' => $summary,
-                'context' => $context,
-                'version' => $version
-            );
+    // Envoi de la requête à l'API
+    $response = wp_remote_post('https://dev-blog-gen.n3web-dev.fr/wp-json/blog-architect/v1/endpoint', array(
+        'method' => 'POST',
+        'headers' => array('Content-Type' => 'application/json'),
+        'body' => json_encode($data),
+        'timeout' => 500,
+    ));
 
-            $categories = wp_get_post_categories($post_id, array('fields' => 'all'));
-            foreach ($categories as $category) {
-                error_log('Category for post ' . $post_id . ': ' . $category->name);
-            }
-            error_log(print_r($data, true));
-
-            /*$response = wp_remote_post('https://dev-blog-gen.n3web-dev.fr/wp-json/blog-architect/v1/endpoint', array(
-                'method' => 'POST',
-                'headers' => array('Content-Type' => 'application/json'),
-                'body' => json_encode($data),
-                'timeout' => 500,
-            ));*/
-
-            $response = wp_remote_post('https://dev.n3web-dev.fr/wp-json/blog-architect/v1/endpoint', array(
-                'method' => 'POST',
-                'headers' => array('Content-Type' => 'application/json'),
-                'body' => json_encode($data),
-                'timeout' => 500,
-            ));
-
-            error_log('Response: ' . print_r($response, true));
-
-            if (!is_wp_error($response) && wp_remote_retrieve_response_code($response) == 200) {
-                $body = wp_remote_retrieve_body($response);
-                $data = json_decode($body, true);
-                error_log('Data2: ' . print_r($data, true));
-                error_log('Data2 ID: ' . $data['id']['post_result']['metas']['title']);
-                if (isset($data['id'])) {
-                    $article_title = $data['id']['post_result']['metas']['title'];
-                    error_log('Article Title: ' . $article_title);
-                    $content = $data['id']['post_result']['content'];
-                    error_log('Content: ' . $content);
-                    $thumbnail = $data['id']['post_result']['thumbnail'];
-                    error_log('Thumbnail: ' . $thumbnail);
-                    $post_article = wp_insert_post(array(
-                        'post_title' => $article_title,
-                        'post_content' => $content,
-                        'post_status' => 'publish',
-                        'post_type' => 'post',
-                    ));
-                    error_log('Post ID: ' . $post_article);
-                    $category_ids = array_map(function($category) {
-                        return $category->term_id;
-                    }, $categories);
-                    wp_set_post_categories($post_article, $category_ids);
-
-                    media_sideload_image($thumbnail, $post_article, $article_title);
-                    $attachments = get_posts(array(
-                        'post_type' => 'attachment',
-                        'numberposts' => 1,
-                        'order' => 'DESC',
-                    ));
-                    set_post_thumbnail($post_article, $attachments[0]->ID);
-
-                }
-                wp_delete_post($post_id, true);
-            }
-
-        }
-        
+    // Vérification de la réponse de l'API
+    if (!is_wp_error($response) && wp_remote_retrieve_response_code($response) == 200) {
+        process_api_response($response, $post_id);
     }
 }
 
- 
-function update_categories_for_selected_posts($selected_posts) {
-    $selected_posts = array_map('intval', $selected_posts);
+/**
+ * Traite la réponse de l'API et gère la création du nouveau post.
+ *
+ * @param array $response Réponse de l'API.
+ * @param int $post_id ID du post d'origine.
+ */
+function process_api_response($response, $post_id) {
+    $body = wp_remote_retrieve_body($response);
+    $data = json_decode($body, true);
 
+    if (isset($data['id'])) {
+        $article_title = $data['id']['post_result']['metas']['title'];
+        $content = $data['id']['post_result']['content'];
+        $thumbnail = $data['id']['post_result']['thumbnail'];
+
+        // Insertion d'un nouveau post
+        $post_article = wp_insert_post(array(
+            'post_title' => $article_title,
+            'post_content' => $content,
+            'post_status' => 'publish',
+            'post_type' => 'post',
+        ));
+
+        // Attribution des catégories au nouveau post
+        $category_ids = array_map(function($category) {
+            return $category->term_id;
+        }, wp_get_post_categories($post_id, array('fields' => 'all')));
+        wp_set_post_categories($post_article, $category_ids);
+
+        // Ajout de l'image à la une
+        media_sideload_image($thumbnail, $post_article, $article_title);
+        $attachments = get_posts(array(
+            'post_type' => 'attachment',
+            'numberposts' => 1,
+            'order' => 'DESC',
+        ));
+        set_post_thumbnail($post_article, $attachments[0]->ID);
+
+        // Suppression du post d'origine
+        wp_delete_post($post_id, true);
+    }
+}
+
+/**
+ * Met à jour les catégories pour les posts sélectionnés.
+ *
+ * @param array $selected_posts IDs des posts sélectionnés.
+ */
+function update_categories_for_selected_posts($selected_posts) {
     $action = sanitize_text_field($_POST['bulk_action']);
 
     if ($action === 'remove_category') {
@@ -119,16 +97,9 @@ function update_categories_for_selected_posts($selected_posts) {
         if ($category_id !== 0) {
             $default_category_id = get_option('default_category');
             foreach ($selected_posts as $post_id) {
-                // Get current categories of the post
                 $current_categories = wp_get_post_categories($post_id, array('fields' => 'ids'));
-
-                // Remove the default category from the array
                 $new_categories = array_diff($current_categories, array($default_category_id));
-
-                // Add the new category to the array
                 $new_categories[] = $category_id;
-
-                // Set the updated categories back to the post
                 wp_set_post_categories($post_id, $new_categories);
             }
         }
@@ -138,6 +109,5 @@ function update_categories_for_selected_posts($selected_posts) {
         }
     }
 }
-
 
 ?>
--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?
 /*
-Plugin Name: article client
+Plugin Name: Générateur d'article - Plugin client
 Plugin URI: https://www.n3web.fr
 Description: Appel generateur article client
 Version: 1.0.0
@@ -8,11 +8,10 @@ Author: N3web
 Author URI: https://www.n3web.fr
 License: GPLv2 or later
 */
-
+// Enregistrement des scripts et styles
 function enqueue_public_scripts() {
     wp_enqueue_script('generateur-articles-public-script', plugin_dir_url(__FILE__) . 'Jscript.js', array('jquery'), '1.0.0', true);
 }
-
 add_action('admin_enqueue_scripts', 'enqueue_public_scripts');
 
 function enqueue_public_styles() {
@@ -20,8 +19,8 @@ function enqueue_public_styles() {
 }
 add_action('wp_enqueue_scripts', 'enqueue_public_styles');
 
+// Création de la page de réglages
 add_action('admin_menu', 'create_plugin_settings_page');
-
 function create_plugin_settings_page() {
     add_menu_page(
         'Client Plugin Settings',
@@ -31,52 +30,43 @@ function create_plugin_settings_page() {
         'client_plugin_settings'
     );
 }
-
 function client_plugin_settings() {
     include 'interface.php';
 }
 
+// Gestion de la soumission de formulaire
 function handle_form_submission() {
+    if (!current_user_can('manage_options')) {
+        wp_die(__('Vous n\'avez pas les permissions nécessaires pour accéder à cette page.'));
+    }
     check_admin_referer('save_api_key_and_version_nonce', 'save_api_key_and_version_nonce_field');
 
-    update_option('option_api_key', $_POST['api_key']);
-    update_option('option_version', $_POST['version']);
-    update_option('option_summary', $_POST['summary']);
+    update_option('option_api_key', sanitize_text_field($_POST['api_key']));
+    update_option('option_version', sanitize_text_field($_POST['version']));
+    update_option('option_summary', sanitize_textarea_field($_POST['summary']));
     wp_redirect(admin_url('admin.php?page=client-plugin'));
     exit;
 }
-
 add_action('admin_post_save_api_key_and_version', 'handle_form_submission');
 
+// Sauvegarde des articles en brouillon
 add_action('admin_post_blog_architect_generate_posts', 'save_as_draft');
-
-
-//add_action('admin_post_blog_architect_handle_bulk_actions', 'update_categories_for_selected_posts');
-
-function save_as_draft(){
+function save_as_draft() {
+    if (!current_user_can('edit_posts')) {
+        wp_die(__('Vous n\'avez pas les permissions nécessaires pour accéder à cette page.'));
+    }
     $h1_list = get_post_titles_from_request();
-    error_log('h1_list: ' . print_r($h1_list, true));
     foreach($h1_list as $h1) {
         save_post_as_draft($h1);
     }
     wp_redirect(admin_url('admin.php?page=client-plugin'));
 }
-
 function get_post_titles_from_request() {
     $post_titles = sanitize_textarea_field($_POST['post_titles']);
-
     return array_filter(array_map('trim', explode("\n", str_replace("\r\n", "\n", $post_titles))));
 }
-
 function save_post_as_draft($h1) {
     $slug = sanitize_title($h1);
-
-    $args = [
-        'post_type' => 'post',
-        'name' => $slug,
-        'posts_per_page' => 1,
-        'fields' => 'ids'
-    ];
 
     $post = [
         'post_title' => $h1,
@@ -85,61 +75,11 @@ function save_post_as_draft($h1) {
     ];
 
     $id = wp_insert_post($post);
-    if (!$id) {  
+    if (is_wp_error($id)) {  
+        error_log('Erreur lors de l\'insertion du post : ' . $id->get_error_message());
         return false;
     }
 
     update_post_meta($id, '_is_ba_post', 1);
-
-    wp_reset_postdata();
     return true;
 }
-/*
-if ($_POST['action'] === 'blog_architect_handle_bulk_actions') {
-    if (isset($_GET['post_ids'])) {
-        $selected_posts = $_GET['post_ids'];
-        error_log('Selected Posts: ' . print_r($selected_posts, true));
-        update_categories_for_selected_posts($selected_posts);
-    }
-}*/
-/*
-function update_categories_for_selected_posts() {
-    $selected_post = $_POST['post_ids'];
-    error_log('Selected Posts: ' . print_r($selected_post, true));
-
-    $selected_posts = array_map('intval', $selected_post);
-
-    $action = sanitize_text_field($_POST['bulk_action']);
-
-    if ($action === 'remove_category') {
-        $default_category_id = get_option('default_category');
-        foreach ($selected_posts as $post_id) {
-            wp_set_post_categories($post_id, array($default_category_id));
-        }
-    } elseif ($action === 'add_category') {
-        $category_id = intval($_POST['category']);
-        if ($category_id !== 0) {
-            $default_category_id = get_option('default_category');
-            foreach ($selected_posts as $post_id) {
-                // Get current categories of the post
-                $current_categories = wp_get_post_categories($post_id, array('fields' => 'ids'));
-
-                // Remove the default category from the array
-                $new_categories = array_diff($current_categories, array($default_category_id));
-
-                // Add the new category to the array
-                $new_categories[] = $category_id;
-
-                // Set the updated categories back to the post
-                wp_set_post_categories($post_id, $new_categories);
-            }
-        }
-    } elseif ($action === 'delete_posts') {
-        foreach ($selected_posts as $post_id) {
-            wp_delete_post($post_id, true);
-        }
-    }
-}
-*/
-
-?>
